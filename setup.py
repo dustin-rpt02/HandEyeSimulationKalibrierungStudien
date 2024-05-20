0c504d884ee27d59248753ea2f0d1d0156c74094
dustin-rpt02 <dustin.rpt02@gmail.com>
2024-05-20 09:52:09 +0200

    retrieve point cloud topic

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'test = hand_eye_simulation_calibration.objekt_erkennung:main'
         ],
     },
 )
